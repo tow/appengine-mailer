06eedeaf59aaf8ceec7582af7be9aec8febc405f
Mat Clayton <mat@wakari.co.uk>
2010-08-09 18:59:51 +0100

    django email backend fix, added args/kwargs so things like password reset works

--- django_email_backend.py
+++ django_email_backend.py
@@ -5,9 +5,9 @@ from gmail import GmailProxy, MessageSendingFailure
 
 
 class GmailBackend(BaseEmailBackend):
-    def __init__(self, fail_silently=False):
+    def __init__(self, fail_silently=False, *args, **kwargs):
         self.gmail_proxy = GmailProxy(settings.SECRET_KEY, settings.EMAIL_APPENGINE_PROXY_URL, fail_silently)
-        super(GmailBackend, self).__init__(fail_silently)
+        super(GmailBackend, self).__init__(fail_silently, *args, **kwargs)
 
     def send_messages(self, messages):
         n = 0
